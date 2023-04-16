809f289ac5501528842435928f212244e1490671
柯仁傑 <@DESKTOP-34DDU9R>
2023-04-16 22:27:24 +0800

    加入專案檔案。

---
+++ homework22/homework22.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int board[3][3];
+    bool win = false;
+    int i, j;
+
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+
+    for (i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+            win = true;
+            break;
+        }
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+            win = true;
+            break;
+        }
+    }
+
+
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+        win = true;
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+        win = true;
+    }
+
+
+    if (win) {
+        cout << "True" << endl;
+    }
+    else {
+        cout << "False" << endl;
+    }
+
+    return 0;
+}
